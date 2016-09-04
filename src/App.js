52d698c168fa90e34ca7465436fa495428279c9c
Farid Safi <farid.lazreq@gmail.com>
2016-09-04 17:07:48 +0200

    simplify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Text,
   View
 } from 'react-native';
 
-export default class MeetupChat extends Component {
+export default class MeetupChat extends React.Component {
   render() {
     return (
-      <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-      }}>
-        <Text style={{
-          fontSize: 20,
-          textAlign: 'center',
-          margin: 10,
-        }}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF'}}>
+        <Text style={{fontSize: 20, textAlign: 'center', margin: 10}}>
           Welcome to React Native!
         </Text>
       </View>
